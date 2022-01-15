4fd01a94a9f399f645c21f46d6c42fab93322317
AhmetSUNA <34107716+AhmetSUNA@users.noreply.github.com>
2022-01-16 00:41:47 +0300

    SQL

---
+++ Ödev9.sql
@@ -0,0 +1,8 @@
+--Cevap1;
+SELECT city.city,country.country FROM city  INNER JOIN country  ON city.country_id = country.country_id
+
+--Cevap2:
+SELECT payment.payment_id,customer.first_name,customer.last_name FROM customer INNER JOIN payment ON customer.customer_id = payment.customer_id
+
+--Cevap3:
+SELECT rental.rental_id,customer.first_name,customer.last_name FROM customer INNER JOIN rental ON customer.customer_id = rental.customer_id
