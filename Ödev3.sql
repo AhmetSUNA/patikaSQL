b422b13e7682de1de9be4d9b774b976e75edf32c
AhmetSUNA <34107716+AhmetSUNA@users.noreply.github.com>
2022-01-15 15:04:32 +0300

    SQL

---
+++ Ödev3.sql
@@ -0,0 +1,11 @@
+--Cevap1:
+SELECT country FROM country WHERE country LIKE 'A%a'
+
+--Cevap2:
+SELECT country FROM country WHERE LENGTH(country) >= 6 AND country LIKE '%n'
+
+--Cevap3:
+SELECT title FROM film WHERE title LIKE '%t%t%t%t%'
+
+--Cevap4:
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
